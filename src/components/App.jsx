83b3dba2df1b2092cfaf0e40d350ede66d51f35c
cYpHeR <connect2cj21@gmail.com>
2020-12-26 17:34:49 +0530

    v- 1.0.0 launched

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import ToDoItem from './toDoItem';
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+  function deleteItem(id)
+  {
+    setItems(prevItems=>
+      {
+        return prevItems.filter((item,idx)=>idx!==id);
+      })
+  }
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem,index) => (<ToDoItem text={todoItem} key={index} id={index} taskDone={deleteItem}/>))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
